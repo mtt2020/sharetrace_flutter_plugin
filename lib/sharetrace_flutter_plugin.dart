a5fa1cc12de633ab04021e5598d02b247726c65d
mtt2020 <matengteng2019@gmail.com>
2021-04-22 14:47:23 +0800

    sharetrace_flutter_plugin

---
+++ lib/sharetrace_flutter_plugin.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+typedef Future<dynamic> ResponseHandler(Map<String, String> data);
+
+class SharetraceFlutterPlugin {
+  static const MethodChannel _channel =
+      const MethodChannel('sharetrace_flutter_plugin');
+
+  static SharetraceFlutterPlugin? _instance;
+  SharetraceFlutterPlugin._internal() {
+    _channel.setMethodCallHandler(_onMethodHandle);
+  }
+  factory SharetraceFlutterPlugin.getInstance() => _getInstance();
+
+  static _getInstance() {
+    _instance ??= SharetraceFlutterPlugin._internal();
+    return _instance;
+  }
+
+  Future defaultHandler() async {}
+
+  ResponseHandler? _installRespHandler;
+  ResponseHandler? _wakeupRespHandler;
+
+  Future _onMethodHandle(MethodCall call) async {
+    if (call.method == "onInstallResponse") {
+      if (_installRespHandler != null) {
+        return _installRespHandler?.call(call.arguments.cast<String, String>());
+      }
+      return defaultHandler();
+    } else if (call.method == "onWakeupResponse") {
+      if (_wakeupRespHandler != null) {
+        return _wakeupRespHandler?.call(call.arguments.cast<String, String>());
+      }
+      return defaultHandler();
+    }
+  }
+
+  void registerWakeupHandler(ResponseHandler responseHandler) {
+    _wakeupRespHandler = responseHandler;
+    _channel.invokeMethod("registerWakeup");
+  }
+
+  void getInstallTrace(ResponseHandler responseHandler) {
+    var args = new Map();
+    this._installRespHandler = responseHandler;
+    _channel.invokeMethod("getInstallTrace", args);
+  }
+}
